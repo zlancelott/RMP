89a7756de014658b63602b1a27bf7ac682a37640
Joao Carlos Prata <johncarlosilver@gmail.com>
2018-09-07 02:24:00 -0300

    FTP Server link 1

---
+++ flaskrepositorio/download_ftp_files.py
@@ -0,0 +1,28 @@
+import os
+from ftplib import FTP
+
+ftp = FTP('177.19.73.31')
+ftp.login(user='FTP_Server', passwd='ftpserver')
+
+ftp.cwd("/Computer Science/7-semester/Teoria da Computação/Aula 01/")
+
+filenames = ftp.nlst()
+
+
+for filename in filenames:
+    # download the file
+    local_filename = os.path.join(os.getcwd() + r"\\static\cache", filename)
+    lf = open(local_filename, "wb")
+    ftp.retrbinary("RETR " + filename, lf.write, 8 * 1024)
+    lf.close()
+
+
+# ## APAGAR ARQUIVOS DEPOIS DO DOWNLOAD ##
+
+# import os
+# arq = os.listdir(os.getcwd() + r"\\static\cache")
+
+# arq = [os.getcwd() + r"\\static\cache\\" + i for i in arq]
+
+# for a in arq:
+#     os.remove(a)
---
+++ flaskrepositorio/teste.py
@@ -0,0 +1,9 @@
+import os
+
+
+filename = "1.jpg"
+
+local_filename = os.path.join(r"\static", filename)
+
+
+print(os.getcwd())
