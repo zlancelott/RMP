0aac66770e46423a133c98d4c9ef0b38ca75f81c
brenoalef <b.alefdsa@gmail.com>
2018-09-14 02:54:49 -0300

    [RMD-7] Removendo import desnecessário.

--- flaskrepositorio/__init__.py
+++ flaskrepositorio/__init__.py
@@ -2,16 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = './flaskrepositorio/uploads'
 
 app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = '704f06afe4b162197fbeb4e1fc2dd9d1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/banco-rmd'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = './flaskrepositorio/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
